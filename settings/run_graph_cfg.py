e963911bd4327f510a3fcdf2a93f83419b2e4612
mamelz <mariusmelz95@googlemail.com>
2023-11-22 00:00:17 +0100

    add config for run graph

---
+++ settings/run_graph_cfg.py
@@ -0,0 +1,127 @@
+RUN_GRAPH_CONFIG_DICT = {
+    "ranks": {
+        "NONE": {
+            "NONE": {}
+        },
+        "Schedule": {
+            "default": {
+                "mandatory": {
+                    "stages": {
+                        "types": (list,)
+                    },
+                }
+            },
+        },
+        "Stage": {
+            "default": {
+                "mandatory": {
+                    "routines": {
+                        "types": (dict,)
+                    },
+                },
+            },
+        },
+        "Routine": {
+            "regular": {
+                "mandatory": {
+                    "routine_name": {
+                        "types": (str,),
+                    },
+                    "args": {
+                        "types": (list,)
+                    },
+                    "description": {
+                        "types": (str,)
+                    },
+                    "kwargs": {
+                        "types": (dict,)
+                    },
+                    "live_tracking": {
+                        "types": (bool,)
+                    },
+                    "store": {
+                        "types": (bool,)
+                    },
+                    "store_token": {
+                        "types": (str,)
+                    },
+                    "tag": {
+                        "types": (str,)
+                    }
+                }
+            },
+            "evolution": {
+                "mandatory": {
+                    "routine_name": {
+                        "types": (str,)
+                    },
+                    "time": {
+                        "types": (float,)
+                    },
+                    "args": {
+                        "types": (list,)
+                    },
+                    "description": {
+                        "types": (str,)
+                    },
+                    "kwargs": {
+                        "types": (dict,)
+                    },
+                    "live_tracking": {
+                        "types": (bool,)
+                    },
+                    "store": {
+                        "types": (bool,)
+                    },
+                    "store_token": {
+                        "types": (str,)
+                    },
+                    "tag": {
+                        "types": (str,)
+                    }
+                }
+            },
+            "propagation": {
+                "mandatory": {
+                    "time": {
+                        "types": (float,)
+                    },
+                    "step": {
+                        "types": (float,)
+                    }
+                }
+            }
+        }
+    },
+    "hierarchy": {
+        "NONE": -1,
+        "Schedule": 0,
+        "Stage": 1,
+        "Routine": 2
+    },
+    "allowed_children": {
+        "NONE": {
+            "NONE": {
+                "Schedule": ("default",)
+            }
+        },
+        "Schedule": {
+            "default": {
+                "Stage": ("default",),
+            }
+        },
+        "Stage": {
+            "regular": {
+                "Routine": ("regular",)
+            },
+            "evolution": {
+                "Routine": ("evolution", "propagation")
+            }
+        },
+        "Routine": {
+            "regular": (),
+            "evolution": (),
+            "propagation": ()
+        }
+    }
+}
