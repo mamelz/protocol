685ce633e9ba8b72f28bd9bdaec39fe1290a064e
mamelz <mariusmelz95@googlemail.com>
2024-03-24 01:39:58 +0000

    remove commented code

--- builder/base.py
+++ builder/base.py
@@ -15,19 +15,3 @@ class GraphProcessor(ABC):
     def __call__(self, input: GraphNodeMeta) -> GraphNodeMeta:
         if not isinstance(input, self.input_type):
             raise self.GraphProcessorError()
-
-#    @classmethod
-#    @property
-#    def specs(cls) -> tuple[GraphSpecification]:
-#        return (cls.userspec, cls.interspec, cls.runspec)
-
-#    def _check(self, graph: GraphRoot):
-#        if graph.graph_spec not in self.specs:
-#            raise self.GraphProcessorError(
-#                "Unknown specification."
-#            )
-#
-#        if not isinstance(graph, GraphRoot):
-#            raise self.GraphProcessorError(
-#                f"Graph {graph} must be GraphRoot instance."
-#            )
